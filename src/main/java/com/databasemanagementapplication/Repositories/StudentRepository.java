be6310e31c88c0857337bfa0d3d54c7339e9f767
UserDead-19 <abinav10102@gmail.com>
2024-05-03 19:34:41 +0530

    chore: Update StudentRepository with @Repository annotation

--- src/main/java/com/databasemanagementapplication/Repositories/StudentRepository.java
+++ src/main/java/com/databasemanagementapplication/Repositories/StudentRepository.java
@@ -1,8 +1,11 @@
 package com.databasemanagementapplication.Repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import com.databasemanagementapplication.Model.Student;
 
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 
 }
