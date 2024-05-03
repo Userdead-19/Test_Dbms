a3079f7fb3888add7c0d144bab2574885ab26089
UserDead-19 <abinav10102@gmail.com>
2024-05-03 19:28:15 +0530

    removed dotenv config in the main class

--- src/main/java/com/databasemanagementapplication/DbmsApplication.java
+++ src/main/java/com/databasemanagementapplication/DbmsApplication.java
@@ -2,10 +2,7 @@ package com.databasemanagementapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.databasemanagementapplication.Controller",
@@ -15,15 +12,7 @@ public class DbmsApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure().load();
-
-		System.setProperty("dotenv", dotenv.toString());
 		SpringApplication.run(DbmsApplication.class, args);
-	}
-
-	@Bean
-	public Dotenv dotenv() {
-		return Dotenv.configure().load();
 	}
 
 }
